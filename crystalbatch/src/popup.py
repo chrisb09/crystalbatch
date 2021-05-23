5c69b97bb9d42fdf1ec082629b9f383092fa317f
Christian Brinkmann <christian.brinkmann09@gmail.com>
2021-05-23 15:03:53 +0200

    add basic popup functionality

---
+++ crystalbatch/src/popup.py
@@ -0,0 +1,35 @@
+from tkinter.messagebox import *
+import tkinter as tk
+
+
+def _create_root():
+    root = tk.Tk()
+    root.overrideredirect(1)
+    root.withdraw()
+    return root
+
+
+def _destroy_root(root):
+    root.destroy()
+
+
+def _helper(function, *args):
+    tmp = _create_root()
+    function(*args)
+    _destroy_root(tmp)
+
+
+def callfunction(function, root, *args):
+    if root is None:
+        _helper(function, *args)
+    else:
+        function(*args)
+
+
+def error(title, message, root=None):
+    callfunction(showerror, root, title, message)
+    # showerror(title, message)
+
+
+def warning(title, message, root=None):
+    callfunction(showwarning, root, title, message)
