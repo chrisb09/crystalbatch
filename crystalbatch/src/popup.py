61a4c884537b0511b00824b2c13f05ae19a63276
Christian Brinkmann <christian.brinkmann09@gmail.com>
2021-05-23 15:13:18 +0200

    add return functionality to popup functions and add missing popup function calls.

--- crystalbatch/src/popup.py
+++ crystalbatch/src/popup.py
@@ -15,21 +15,46 @@ def _destroy_root(root):
 
 def _helper(function, *args):
     tmp = _create_root()
-    function(*args)
+    res = function(*args)
     _destroy_root(tmp)
+    return res
 
 
 def _callfunction(function, root, *args):
     if root is None:
-        _helper(function, *args)
+        return _helper(function, *args)
     else:
-        function(*args)
+        return function(*args)
 
 
 def error(title, message, root=None):
-    _callfunction(showerror, root, title, message)
+    return _callfunction(showerror, root, title, message)
     # showerror(title, message)
 
 
 def warning(title, message, root=None):
-    _callfunction(showwarning, root, title, message)
+    return _callfunction(showwarning, root, title, message)
+
+
+def info(title, message, root=None):
+    return _callfunction(showinfo, root, title, message)
+
+
+def yes_no_cancel(title, message, root=None):
+    return _callfunction(askyesnocancel, root, title, message)
+
+
+def yes_no(title, message, root=None):
+    return _callfunction(askyesno, root, title, message)
+
+
+def retry_cancel(title, message, root=None):
+    return _callfunction(askretrycancel, root, title, message)
+
+
+def question(title, message, root=None):
+    return _callfunction(askquestion, root, title, message)
+
+
+def okay_cancel(title, message, root=None):
+    return _callfunction(askokcancel, root, title, message)
