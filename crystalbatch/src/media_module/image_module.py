a630115f9fbcdb4f25d62b022b309b25cb034309
Christian Brinkmann <christian.brinkmann09@gmail.com>
2021-05-24 18:33:17 +0200

    format code with black command

--- crystalbatch/src/media_module/image_module.py
+++ crystalbatch/src/media_module/image_module.py
@@ -1,24 +1,25 @@
 from media_module import MediaModule
 import media_module
 
-class ImageModule(MediaModule):
 
+class ImageModule(MediaModule):
     def get_file_extensions(self):
-        return ["png","jpg","jpeg","bmp","gif"]
- 
+        return ["png", "jpg", "jpeg", "bmp", "gif"]
+
     def get_module_name(self):
         return "Image"
- 
+
     def get_module_description(self):
         return "Display all sort of image files including animated gifs."
- 
+
     def check_requirements(self):
         return False
- 
+
     def render(self):
         pass
 
     def update_window_size(self):
         pass
+
 
 media_module.media_modules.append(ImageModule())
